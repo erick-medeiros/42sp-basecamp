9afed2fcd09493f797d5a33ab2c00d8bea8a81b8
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-02-02 18:23:06 +0100

    file avaliate shell00 and shell01

---
+++ evaluations/shell00.sh
@@ -0,0 +1,17 @@
+#ex00
+cat z
+#ex01
+tar -xf testShell00.tar
+ls -l
+#ex02
+tar -xf exo2.tar
+ls -l
+#ex03
+#ex04
+bash git_commit.sh | cat -e
+#ex05
+bash git_ignore.sh | cat -e
+#ex06
+#ex07
+touch a~ \#a# a#
+#ex08
---
+++ evaluations/shell01.sh
@@ -0,0 +1,19 @@
+#ex01
+export FT_USER=coder
+./print_groups.sh
+#ex02
+./find_sh.sh | cat -e
+#ex03
+./count_files.sh | cat -e
+#ex04
+./MAC.sh | cat -e
+#ex05
+ls -lRa *MaRV* | cat -e
+#ex06
+./skip.sh
+ls -l
+#ex07
+export FT_LINE1=
+export FT_LINE2=
+./r_dwssap.sh
+#ex08
