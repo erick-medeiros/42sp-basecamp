a0ae6c79c4f639ed67e2657e9db5f29d499534a1
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-02-06 14:55:30 +0100

    refactor

--- c_00/ex05/ft_print_comb.c
+++ c_00/ex05/ft_print_comb.c
@@ -6,67 +6,35 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 19:36:04 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/02/04 03:23:58 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/02/06 14:00:39 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	write_number(int number)
-{
-	int	ascii_0;
-	int	ascii_number;
-
-	ascii_0 = 48;
-	ascii_number = number + ascii_0;
-	write(1, &ascii_number, 1);
-	return ;
-}
-
-void	show_list(int d1, int d2, int d3)
-{
-	int	ascii_breakline;
-
-	ascii_breakline = 10;
-	if (d1 != d2 && d1 != d3 && d2 != d3)
-	{	
-		if (d1 < d2 && d2 < d3)
-		{
-			write_number(d1);
-			write_number(d2);
-			write_number(d3);
-			if (d1 == 7 && d2 == 8 && d3 == 9)
-			{
-				write(1, &ascii_breakline, 1);
-				return ;
-			}
-			write(1, ", ", 2);
-		}
-	}
-	return ;
-}
-
 void	ft_print_comb(void)
 {
-	int	d1;
-	int	d2;
-	int	d3;
+	int	d[3];
+	int	i;
 
-	d1 = 0;
-	while (d1 <= 9)
+	i = 0;
+	while (i >= 0)
 	{
-		d2 = 0;
-		while (d2 <= 9)
-		{
-			d3 = 0;
-			while (d3 <= 9)
-			{
-				show_list(d1, d2, d3);
-				d3++;
-			}
-			d2++;
+		d[2] = '0' + (i % 10);
+		d[1] = '0' + (((i - (i % 10)) / 10) % 10);
+		d[0] = '0' + (((i - (((i - (i % 10)) / 10) % 10)) / 10 / 10) % 10);
+		if (d[0] < d[1] && d[1] < d[2])
+		{	
+			write(1, &d[0], 1);
+			write(1, &d[1], 1);
+			write(1, &d[2], 1);
+			if (d[0] == '7' && d[1] == '8' && d[2] == '9')
+				break ;
+			else
+				write(1, ", ", 2);
 		}
-		d1++;
+		i++;
 	}
+	write(1, "\n", 1);
 	return ;
 }
