b7807632e24fa1bd93334137b631739e09e4c335
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-02-08 12:51:38 +0100

    evaluate shell01

--- evaluations/shell01.sh
+++ evaluations/shell01.sh
@@ -1,22 +1,39 @@
+clear
 #ex01
+echo "ex01----------------"
 export FT_USER=coder
-chmod 744 
+chmod 744 ./ex01/print_groups.sh
 ./ex01/print_groups.sh
 #ex02
-./find_sh.sh | cat -e
+echo "ex02----------------"
+chmod 744 ./ex02/find_sh.sh
+./ex02/find_sh.sh | cat -e
 #ex03
-./count_files.sh | cat -e
+echo "ex03----------------"
+chmod 744 ./ex03/count_files.sh
+./ex03/count_files.sh | cat -e
 #ex04
-./MAC.sh | cat -e
+echo "ex04----------------"
+chmod 744 ./ex04/MAC.sh
+./ex04/MAC.sh | cat -e
 #ex05
+echo "ex05----------------"
+cd ex05
 ls -lRa *MaRV* | cat -e
+cd ..
 #ex06
-./skip.sh
-ls -l
+echo "ex06----------------"
+chmod 744 ./ex06/skip.sh
+./ex06/skip.sh
 #ex07
-export FT_LINE1=
-export FT_LINE2=
-./r_dwssap.sh
+echo "ex07----------------"
+export FT_LINE1=7
+export FT_LINE2=15
+chmod 744 ./ex07/r_dwssap.sh
+./ex07/r_dwssap.sh
 #ex08
-export FT_LINE2=
-export FT_LINE2=
+echo "ex08----------------"
+export FT_NBR1='\"\"!\"\"!\"\"!\"\"!\"\"!\"\"'
+export FT_NBR2='dcrcmcmooododmrrrmorcmcrmomo'
+chmod 744 ./ex08/add_chelou.sh
+./ex08/add_chelou.sh
