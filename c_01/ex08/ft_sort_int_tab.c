f7bd0534e57d7f4c3e7eecb8032c0bb3b499d9b3
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-02-10 18:50:06 +0100

    fix c01

--- c_01/ex08/ft_sort_int_tab.c
+++ c_01/ex08/ft_sort_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 01:14:36 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/02/10 01:36:57 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/02/10 18:41:27 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	ft_sort_int_tab(int *tab, int size)
 	while (a < size)
 	{
 		b = 0;
-		while (b < size - a)
+		while (b < size - a - 1)
 		{
 			if (tab[b] > tab[b + 1])
 			{
